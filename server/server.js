66fb3842f95ff9675b406f0d37c3c1841db3bfd3
alvin-conrad <alvinaloya90@gmail.com>
2024-03-11 19:57:21 +0800

    first

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+
+app.use(cors());
+
+
+const server = http.createServer(app);
+
+const io = new Server(server , {
+    cors: {
+        origin:"http://localhost:300",
+        methods: ["GET", "POST"],
+
+    }
+})
+
+io.on("connection",(socket) => {
+    console.log(socket.id);
+    socket.on("disconnect", () => {
+        console.log("User is disconnected" , socket.id);
+    })
+})
+
+server.listen(3001 , () => {
+    console.log("Server is running");
+});
